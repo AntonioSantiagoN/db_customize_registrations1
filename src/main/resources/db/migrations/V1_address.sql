4ea94abb479c8b40146dfce03c9becde85341754
AntonioSantiagoN <Reneki2903@outlook.com>
2023-05-22 18:14:45 -0300

    commit inicial do projeto

---
+++ src/main/resources/db/migrations/V1_address.sql
@@ -0,0 +1,6 @@
+use db_customer_registration;
+create table `state` (
+    id bigint not null auto_increment,
+    name varchar(80) not null,
+    primary key (id)
+) engine=InnoDB default charset=utf8;
